b79a8dd98b5b6de3b5a4d1d89a485f8c23676fee
Prakhar <agrawalprakhar031@gmail.com>
2023-08-06 20:59:45 +0530

    create bookform and modify css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import BookContextProvider from "./contexts/Bookcontext";
 import Navbar from "./components/Navbar";
 import BookList from "./components/BookList";
+import BookForm from "./components/BookForm";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,6 +12,7 @@ function App() {
       <BookContextProvider>
         <Navbar/>
         <BookList/>
+        <BookForm/>
       </BookContextProvider>
     </div>
   );
