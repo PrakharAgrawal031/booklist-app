2936b879610e966395a75238b41904bbc4596a69
Prakhar <agrawalprakhar031@gmail.com>
2023-08-06 17:56:21 +0530

    create and add components, design navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import BookContextProvider from "./contexts/Bookcontext";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <BookContextProvider>
-        
+        <Navbar/>
       </BookContextProvider>
     </div>
   );
